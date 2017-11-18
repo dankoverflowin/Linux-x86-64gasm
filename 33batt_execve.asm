5d80b0f4611692388bc968b5f570081ecda9aea5
VawkeIO <io.vawke@gmail.com>
2017-11-18 13:31:25 +0100

    ref1

--- 33batt_execve.asm
+++ 33batt_execve.asm
@@ -1,22 +1,21 @@
 .text
 	.globl _start
 _start:
-	
 	xorq	%rdx, %rdx
 	movq	$0x68732f6e69622fff,%rbx
-	shr	$0x8, %rbx
+	shr	    $0x8, %rbx
 	push	%rbx
 	movq	%rsp,%rdi
 	xorq	%rax,%rax
 	pushq	%rax
 	pushq	%rdi
 	movq	%rsp,%rsi
-	mov	$0x3b,%al	
+	mov	    $0x3b,%al	
 	syscall
 	pushq	$0x1
-	pop	%rdi
+	pop	    %rdi
 	pushq	$0x3c		
-	pop	%rax
+	pop	    %rax
 	syscall
 
 
